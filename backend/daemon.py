baf07f7226e616933d6a307f7980311c1a16dbb2
Calvin Owens <jcalvinowens@gmail.com>
2023-03-31 13:01:01 -0700

    Use multiprocessing instead of threading
    We need to use Process() instead of Thread(), so the interpreter is
    forked and we can actually handle requests in parallel. Also, sieze
    the oppurtunity to clean up the mess of globals.

--- backend/daemon.py
+++ backend/daemon.py
@@ -22,7 +22,7 @@ import socket
 import sqlite3
 import ssl
 import subprocess
-import threading
+import multiprocessing
 import time
 import traceback
 
@@ -46,58 +46,6 @@ import plots
 # If you use self-signed certificates, use the cert from (1) as cert.pem in the
 # source directory (main/). If you use an authority, use their root cert chain.
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--fw", type=str, help="Path to FW binary", required=True)
-parser.add_argument("--elf", type=str, help="Path to ELF file", required=True)
-parser.add_argument("--dst", type=str, help="Server hostname", required=True)
-parser.add_argument("--ssl-cert", type=str, help="SSL Certificate", required=True)
-parser.add_argument("--ssl-key", type=str, help="SSL Key", required=True)
-parser.add_argument("--locations-file", type=str, help="Locations JSON", required=True)
-parser.add_argument("--port", type=int, help="Server port", default=443)
-parser.add_argument("--drop-uid", type=int, help="UID/GID to drop to")
-parser.add_argument("--threads", type=int, help="Thread count", default=16)
-ARGS = parser.parse_args()
-
-with open(ARGS.elf, "rb") as f:
-	CURRENT_FW_SHA = hashlib.sha256(f.read()).hexdigest()[:16]
-
-with open(ARGS.fw, "rb") as f:
-	CURRENT_FW_BLOB = f.read()
-
-DELAYSTEP_US = 100 * 1000
-DB_WRITE_LOCK = threading.Lock()
-LOCS_DICT_LOCK = threading.Lock()
-LOCATIONS_MTIME = 0
-LOCATIONS = {}
-DELAYS = {}
-
-LISTEN_QUEUE_LEN = 16
-ADDR = ('', ARGS.port)
-
-def lookup_location(hw_serial):
-	"""
-	Cache the contents of locations.json based on its mtime.
-
-	This also takes care of assigning each sensor a unique send_delay value
-	by simply counting up from zero in dict order.
-	"""
-	global LOCATIONS, LOCATIONS_MTIME, DELAYS
-
-	with LOCS_DICT_LOCK:
-		if os.stat(ARGS.locations_file).st_mtime != LOCATIONS_MTIME:
-			with open(ARGS.locations_file, "rb") as f:
-				LOCATIONS = json.loads(f.read())
-				LOCATIONS_MTIME = os.fstat(f.fileno()).st_mtime
-				DELAYS = {
-					k: DELAYSTEP_US * i for i, k
-					in enumerate(LOCATIONS.keys())
-				}
-
-		return (
-			LOCATIONS.get(hw_serial, "(unknown)"),
-			DELAYS.get(hw_serial, 0),
-		)
-
 def date_parse(string):
 	"""
 	Parse lots of relative date strings. Nothing available in python is as
@@ -107,83 +55,6 @@ def date_parse(string):
 		["date", "+%s", "-d", string], stderr=subprocess.DEVNULL,
 		stdin=subprocess.DEVNULL, shell=False,
 	).decode('ascii', 'ignore').rstrip())
-
-def init_db():
-	con = sqlite3.connect("data.db")
-	cur = con.cursor()
-	cur.execute("""
-	CREATE TABLE IF NOT EXISTS data(
-		location, c_epoch, s_epoch,
-		r_temp, temp,
-		r_humidity, humidity
-	);
-	""")
-	cur.execute("""
-	CREATE TABLE IF NOT EXISTS submissions(
-		fw_sha, hw_serial, s_epoch, src_ip, ap_rssi,
-		wake_reason, location, retries, wifi_retries,
-		nr_samples
-	);
-	""")
-	con.commit()
-	con.close()
-
-def insert_db(fw_sha, hw_serial, s_epoch, src_ip, ap_rssi, wake_reason,
-	      location, retries, wifi_retries, samples_arr):
-	"""
-	Insert a submission and its associated samples into the database.
-	"""
-	with DB_WRITE_LOCK:
-		con = sqlite3.connect("data.db")
-		cur = con.cursor()
-		cur.execute(f"""
-		INSERT INTO submissions VALUES(
-			'{fw_sha}', '{hw_serial}', {s_epoch}, '{src_ip}',
-			{ap_rssi}, '{wake_reason}', '{location}', {retries},
-			{wifi_retries}, {len(samples_arr)}
-		);
-		""")
-
-		for d in samples_arr:
-			c_epoch = int(d["epoch"])
-			r_temp = d["temperature"]
-			r_humi = d["humidity"]
-
-			# Temperature: 14-bit Resolution (unit 0.01007080078C)
-			# 16-digit result, with two least sig bits always zero
-			# Raw mapping: [0, 16384] == [-40C, 125C]
-			f_temp = round(float(r_temp) / 65536.0 * 165.0 - 40.0, 2)
-
-			# Humidity: 14-bit Resolution (unit 0.00610351562 %RH)
-			# 16-digit result, with two least sig bits always zero
-			# Raw mapping: [0, 16384] == [0%, 100%]
-			f_humi = round(float(r_humi) / 65536.0 * 100.0, 2)
-
-			cur.execute(f"""
-			INSERT INTO data VALUES(
-				'{location}', {c_epoch}, {s_epoch},
-				{r_temp >> 2}, {f_temp},
-				{r_humi >> 2}, {f_humi}
-			);
-			""")
-
-		con.commit()
-		con.close()
-
-def get_db_locations(epoch_start, epoch_end):
-	"""
-	Dump all locations that have data between start/end.
-	"""
-	con = sqlite3.connect("data.db")
-	cur = con.cursor()
-	cur.execute(f"""
-	SELECT location FROM data WHERE c_epoch >= {epoch_start}
-	and c_epoch <= {epoch_end} GROUP BY location;
-	""")
-	rows = cur.fetchall()
-	con.close()
-
-	return [x[0] for x in rows]
 
 class SensorRequestHandler(BaseHTTPRequestHandler):
 	def send_error(self, code, explain=""):
@@ -207,7 +78,7 @@ class SensorRequestHandler(BaseHTTPRequestHandler):
 		]
 
 		if locs is None or (len(locs) == 1 and locs[0] == ''):
-			locs = get_db_locations(start_epoch, end_epoch)
+			locs = self.server.get_db_locations(start_epoch, end_epoch)
 
 		for i, loc in enumerate(locs):
 			xarr.append(plots.gnuplot_plot(loc, "temp", start_epoch,
@@ -226,7 +97,7 @@ class SensorRequestHandler(BaseHTTPRequestHandler):
 		]
 
 		if locs is None or (len(locs) == 1 and locs[0] == ''):
-			locs = get_db_locations(start_epoch, end_epoch)
+			locs = self.server.get_db_locations(start_epoch, end_epoch)
 
 		for i, loc in enumerate(locs):
 			xarr.append(plots.gnuplot_plot(loc, "humidity", start_epoch,
@@ -307,15 +178,15 @@ class SensorRequestHandler(BaseHTTPRequestHandler):
 				return
 
 		elif self.path.startswith("/fw/"):
-			if not self.path.endswith(CURRENT_FW_SHA):
+			if not self.path.endswith(self.server.fw_sha):
 				self.send_error(404)
 				return
 
 			self.send_response(200)
-			self.send_header("Content-Length", len(CURRENT_FW_BLOB))
+			self.send_header("Content-Length", len(self.server.fw_blob))
 			self.send_header("Content-Type", "application/octet-stream")
 			self.end_headers()
-			self.wfile.write(CURRENT_FW_BLOB)
+			self.wfile.write(self.server.fw_blob)
 			return
 
 		self.send_error(403)
@@ -356,15 +227,15 @@ class SensorRequestHandler(BaseHTTPRequestHandler):
 				"queue_count": 5,
 			}
 
-			location, want_send_delay_us = lookup_location(ser)
+			location, want_send_delay_us = self.server.lookup_location(ser)
 
 			if send_delay_us != want_send_delay_us:
 				rjsond["next_send_delay_us"] = want_send_delay_us
 
-			if sha != CURRENT_FW_SHA:
-				rjsond["new_fw"] = f"https://{ARGS.dst}:{ARGS.port}/fw/{CURRENT_FW_SHA}"
+			if sha != self.server.fw_sha:
+				rjsond["new_fw"] = self.server.fw_url
 
-			insert_db(sha, ser, rxtime,
+			self.server.insert_db(sha, ser, rxtime,
 				  ":".join(str(x) for x in self.client_address),
 				  ap_rssi, wake_reason, location, retries,
 				  wifi_retries, data)
@@ -379,25 +250,156 @@ class SensorRequestHandler(BaseHTTPRequestHandler):
 
 		self.send_error(403)
 
-class ListenThread(threading.Thread):
-	def __init__(self, sock):
-		super().__init__()
-		self.daemon = True
-		self.sock = sock
-		self.start()
+class SensorHTTPServer(HTTPServer):
+	def __init__(self, server_address, request_handler_class, db_path,
+		     db_lock, locs_path, locs_lock, fw_sha, fw_blob, fw_url):
+		super().__init__(server_address, request_handler_class,
+				 bind_and_activate=False)
 
-	def run(self):
-		httpd = HTTPServer(ADDR, SensorRequestHandler,
-				   bind_and_activate=False)
+		self.db_path = db_path
+		self.db_lock = db_lock
+		self.locs_path = locs_path
+		self.locs_lock = locs_lock
+		self.fw_sha = fw_sha
+		self.fw_blob = fw_blob
+		self.fw_url = fw_url
+		self.LOCS_MTIME = 0
 
-		sslctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-		sslctx.load_cert_chain(
-			certfile=ARGS.ssl_cert,
-			keyfile=ARGS.ssl_key,
+	def init_db(self):
+		with self.db_lock:
+			con = sqlite3.connect(self.db_path)
+			cur = con.cursor()
+			cur.execute("""
+			CREATE TABLE IF NOT EXISTS data(
+				location, c_epoch, s_epoch,
+				r_temp, temp,
+				r_humidity, humidity
+			);
+			""")
+			cur.execute("""
+			CREATE TABLE IF NOT EXISTS submissions(
+				fw_sha, hw_serial, s_epoch, src_ip, ap_rssi,
+				wake_reason, location, retries, wifi_retries,
+				nr_samples
+			);
+			""")
+			con.commit()
+			con.close()
+
+	def lookup_location(self, hw_serial):
+		"""
+		Cache the contents of locations.json based on its mtime.
+
+		This also takes care of assigning each sensor a unique
+		send_delay value by simply counting up from zero.
+		"""
+		with self.locs_lock:
+			if os.stat(self.locs_path).st_mtime != self.LOCS_MTIME:
+				with open(self.locs_path, "rb") as f:
+					self.LOCS = json.loads(f.read())
+					self.LOCS_MTIME = os.fstat(f.fileno()).st_mtime
+					self.LOCS_DELAYS = {
+						k: 100 * 1000 * i for i, k
+						in enumerate(self.LOCS.keys())
+					}
+
+		return (
+			self.LOCS.get(hw_serial, "(unknown)"),
+			self.LOCS_DELAYS.get(hw_serial, 0),
 		)
 
-		httpd.socket = sslctx.wrap_socket(self.sock, server_side=True)
-		httpd.serve_forever()
+	def get_db_locations(self, epoch_start, epoch_end):
+		"""
+		Dump all locations that have data between start/end.
+		"""
+		with self.db_lock:
+			con = sqlite3.connect(self.db_path)
+			cur = con.cursor()
+			cur.execute(f"""
+			SELECT location FROM data WHERE c_epoch >= {epoch_start}
+			and c_epoch <= {epoch_end} GROUP BY location;
+			""")
+			rows = cur.fetchall()
+			con.close()
+
+			return [x[0] for x in rows]
+
+	def insert_db(self, fw_sha, hw_serial, s_epoch, src_ip, ap_rssi,
+		      wake_reason, location, retries, wifi_retries, samples_arr):
+		"""
+		Insert a submission and its associated samples into the database.
+		"""
+		with self.db_lock:
+			con = sqlite3.connect(self.db_path)
+			cur = con.cursor()
+			cur.execute(f"""
+			INSERT INTO submissions VALUES(
+				'{fw_sha}', '{hw_serial}', {s_epoch}, '{src_ip}',
+				{ap_rssi}, '{wake_reason}', '{location}', {retries},
+				{wifi_retries}, {len(samples_arr)}
+			);
+			""")
+
+			for d in samples_arr:
+				c_epoch = int(d["epoch"])
+				r_temp = d["temperature"]
+				r_humi = d["humidity"]
+
+				# Temperature: 14-bit Resolution (unit 0.01007080078C)
+				# 16-digit result, with two least sig bits always zero
+				# Raw mapping: [0, 16384] == [-40C, 125C]
+				f_temp = round(float(r_temp) / 65536.0 * 165.0 - 40.0, 2)
+
+				# Humidity: 14-bit Resolution (unit 0.00610351562 %RH)
+				# 16-digit result, with two least sig bits always zero
+				# Raw mapping: [0, 16384] == [0%, 100%]
+				f_humi = round(float(r_humi) / 65536.0 * 100.0, 2)
+
+				cur.execute(f"""
+				INSERT INTO data VALUES(
+					'{location}', {c_epoch}, {s_epoch},
+					{r_temp >> 2}, {f_temp},
+					{r_humi >> 2}, {f_humi}
+				);
+				""")
+
+			con.commit()
+			con.close()
+
+def worker(db_lock, locs_lock, addr, ssl_cert, ssl_key, fw_sha, fw_blob, fw_url,
+	   sock):
+	httpd = SensorHTTPServer(addr, SensorRequestHandler,
+				 "data.db", db_lock,
+				 "locations.json", locs_lock,
+				 fw_sha, fw_blob, fw_url)
+
+	sslctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+	sslctx.load_cert_chain(
+		certfile=ssl_cert,
+		keyfile=ssl_key,
+	)
+
+	httpd.init_db()
+	httpd.socket = sslctx.wrap_socket(sock, server_side=True)
+	httpd.serve_forever()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--fw", type=str, help="Path to FW binary", required=True)
+parser.add_argument("--elf", type=str, help="Path to ELF file", required=True)
+parser.add_argument("--dst", type=str, help="Server hostname", required=True)
+parser.add_argument("--ssl-cert", type=str, help="SSL Certificate", required=True)
+parser.add_argument("--ssl-key", type=str, help="SSL Key", required=True)
+parser.add_argument("--locations-file", type=str, help="Locations JSON", required=True)
+parser.add_argument("--port", type=int, help="Server port", default=443)
+parser.add_argument("--drop-uid", type=int, help="UID/GID to drop to")
+parser.add_argument("--threads", type=int, help="Thread count", default=16)
+ARGS = parser.parse_args()
+
+with open(ARGS.elf, "rb") as f:
+	CURRENT_FW_SHA = hashlib.sha256(f.read()).hexdigest()[:16]
+
+with open(ARGS.fw, "rb") as f:
+	CURRENT_FW_BLOB = f.read()
 
 socks = [
 	socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -406,13 +408,28 @@ socks = [
 
 for sock in socks:
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-	sock.bind(ADDR)
-	sock.listen(LISTEN_QUEUE_LEN)
+	sock.bind(('', ARGS.port))
+	sock.listen(10)
 
 if os.getuid() == 0 and ARGS.drop_uid:
 	os.setgid(ARGS.drop_uid)
 	os.setuid(ARGS.drop_uid)
 
-init_db()
+database_write_lock = multiprocessing.Lock()
+locations_file_lock = multiprocessing.Lock()
+
 for sock in socks:
-	ListenThread(sock)
+	multiprocessing.Process(
+		target=worker,
+		args=(
+			database_write_lock,
+			locations_file_lock,
+			('', ARGS.port),
+			ARGS.ssl_cert,
+			ARGS.ssl_key,
+			CURRENT_FW_SHA,
+			CURRENT_FW_BLOB,
+			f"https://{ARGS.dst}:{ARGS.port}/fw/{CURRENT_FW_SHA}",
+			sock,
+		),
+	).start()
